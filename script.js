dd862b204bb06589d1d78647744c79b709905419
Justin Reiser <128163422+jwreiser@users.noreply.github.com>
2023-05-15 12:36:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function scrollHeader() {
   }
 }
 
-var aboutMe='\<section id="about-me" class="about-container"\>";
+var aboutMe='\<section id=\"about-me\" class=\"about-container\"\>";
 /*
       \<div id="about-image"\>
         \<img src="https://res.cloudinary.com/da81thphx/image/upload/v1683979645/profile_nn5wab.gif" class="profile_image"/\>
