5a57f0cbbcb8ba54c5d591b74b1c4fc605d8de45
Justin Reiser <128163422+jwreiser@users.noreply.github.com>
2023-05-15 13:02:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -51,12 +51,14 @@ function scrollHeader() {
   }
 }
 
-var aboutMe='\<section id="about-me" class="about-container"\> \<div id="about-image"\> 
-\</section\>';
+var aboutMe='\<section id="about-me" class="about-container"\> 
+-\<div id="about-image"\> 
+  - \<img src="https://res.cloudinary.com/da81thphx/image/upload/v1683979645/profile_nn5wab.gif" class="profile_image"/\>
+   -   </div>
+-\</section\>';
 /*
      
-        \<img src="https://res.cloudinary.com/da81thphx/image/upload/v1683979645/profile_nn5wab.gif" class="profile_image"/\>
-      </div>
+       
 
       \<div id="about-text"\>
           \<h1\>
