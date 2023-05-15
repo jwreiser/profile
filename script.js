34ab1146e69e230257fc0e80140a5bc640d33410
Justin Reiser <128163422+jwreiser@users.noreply.github.com>
2023-05-15 12:27:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,8 @@ function scrollHeader() {
   }
 }
 
-var aboutMe='\<section id="about-me" class="about-container"\>
+var aboutMe='\<section id="about-me" class="about-container"\>";
+/*
       \<div id="about-image"\>
         \<img src="https://res.cloudinary.com/da81thphx/image/upload/v1683979645/profile_nn5wab.gif" class="profile_image"/\>
       </div>
@@ -66,6 +67,7 @@ var aboutMe='\<section id="about-me" class="about-container"\>
           </p\>
       \</div\>
     \</section\>';
+    */
 var totalScroll=0;
 function extraScroll(elementId) {
     document.getElementById("content-area").innerHTML=aboutMe;
